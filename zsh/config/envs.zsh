cdea0e258b9ba08e42a4765518ca7e9fdba6e5cf
yuluo-yx <yuluo08290126@gmail.com>
2025-12-28 13:03:09 +0800

    chore
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- zsh/config/envs.zsh
+++ zsh/config/envs.zsh
@@ -17,3 +17,12 @@ export AI_DASHSCOPE_API_KEY="sk-xxxx"
 
 # zsh 相关的工具会放在家目录下，因此将家目录加入到 PATH 中
 export PATH=${HOME}/.local/bin:$PATH
+
+# fzf
+export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
+
+# AI envs
+AI_DASHSCOPE_API_KEY="xxxx"
+
+# eza envs
+export FPATH="/Users/${USER}/.oh-my-zsh/custom/plugins/ezacompletions/zsh:$FPATH"
