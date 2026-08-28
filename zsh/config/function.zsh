2b2d9f2f2810e3f0262d9e8c154ddf98bd25b682
shown <1481556636@qq.com>
2026-08-28 11:33:13 +0800

    Implement Ghostty title bar functionality
    Add Ghostty title bar configuration to update terminal title dynamically.

--- zsh/config/function.zsh
+++ zsh/config/function.zsh
@@ -1,7 +1,22 @@
 # Function Plugin config
 
+# Ghostty 标题栏配置
+if [[ -n "${GHOSTTY_RESOURCES_DIR:-}" ]]; then
+    ghostty_set_title() {
+        # 将 HOME 替换为 ~，保持标题更短
+        local dir="${PWD/#$HOME/~}"
+        # Ghostty 支持 OSC 2 设置窗口标题
+        printf '\033]2;%s\033\\' "$dir"
+    }
+
+    autoload -Uz add-zsh-hook
+    add-zsh-hook chpwd ghostty_set_title
+    add-zsh-hook precmd ghostty_set_title
+    add-zsh-hook preexec ghostty_set_title
+    ghostty_set_title
+fi
+
 # Yazi
-# 目前为止还没有用起来...
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
