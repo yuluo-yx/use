6eb9e3f6c0b9a116801357f1ba0f1dc5f628f839
shown <1481556636@qq.com>
2026-08-30 12:27:46 +0800

    Update fzf.zsh

--- zsh/config/fzf.zsh
+++ zsh/config/fzf.zsh
@@ -1,10 +1,31 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */Users/${USER}/.oh-my-zsh/custom/plugins/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/${USER}/.oh-my-zsh/custom/plugins/fzf/bin"
-fi
+# =========================================================
+# fzf
+# =========================================================
 
-export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --strip-cwd-prefix'  # strip-cwd-prefix removes the leading ./ from results
 
-# 如果出现 unknown option --zsh，需要升级 fzf 版本
-source <(fzf --zsh)
+# Ctrl-T uses fd
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# UI
+export FZF_DEFAULT_OPTS='
+  --height=60%
+  --layout=reverse
+  --border=rounded
+  --prompt="  "
+  --pointer="  "
+  --preview-window=right:65%:wrap:border-left
+'
+
+export _FZF_PREVIEW_CMD='bat --color=always --style=plain,numbers --line-range=:500 {}'
+export FZF_CTRL_T_OPTS="--preview '$_FZF_PREVIEW_CMD'"
+
+# Ctrl+F: file picker excluding hidden files
+_fzf_file_no_hidden() {
+  local cmd result
+  cmd="${FZF_DEFAULT_COMMAND/--hidden /}"
+  result=$(eval "${cmd:-find . -type f}" | fzf --preview "$_FZF_PREVIEW_CMD") \
+    && LBUFFER+="$result"  # LBUFFER is the text left of the cursor
+  zle reset-prompt
+}
+zle -N _fzf_file_no_hidden
